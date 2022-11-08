b9da9e9043bee51e88119c9cdc1a75fc5958da14
Brodin Collins-Robb <Bcollinsrobb9037@conestogac.on.ca>
2022-11-08 11:55:33 -0500

    Created unit test project

---
+++ BodyMachineTests/BodyMachineTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BodyMachineTests
+{
+	TEST_CLASS(BodyMachineTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
