671054375ce303c5b3fb8b2cdd09548069cc8819
Tyler Scheifley <tylerscheifley@gmail.com>
2022-11-08 11:44:57 -0500

    Created Unit Test File

---
+++ PaintMachine Tests/PaintMachine Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PaintMachineTests
+{
+	TEST_CLASS(PaintMachineTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
