0b11f60bc4093ae9241f553b0c3a9a6fa5852169
Owen Garland <Ogarland9238@conestogac.on.ca>
2022-11-08 11:40:11 -0500

    Test Files Created

---
+++ VehicleTests/VehicleTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace VehicleTests
+{
+	TEST_CLASS(VehicleTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
