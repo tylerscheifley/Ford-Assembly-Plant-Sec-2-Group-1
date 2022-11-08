b66f560bfbb07292a73bca42b503a9e7a585eb80
Brodin Collins-Robb <Bcollinsrobb9037@conestogac.on.ca>
2022-11-08 11:52:30 -0500

    Added test project

---
+++ InteriorMachineTests/InteriorMachineTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace InteriorMachineTests
+{
+	TEST_CLASS(InteriorMachineTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
