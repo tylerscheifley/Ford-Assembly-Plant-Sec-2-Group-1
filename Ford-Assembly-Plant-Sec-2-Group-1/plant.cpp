7ec698d5151c714409bc93430411798a0a0b5003
ZNLake <91035765+ZNLake@users.noreply.github.com>
2022-11-10 12:01:46 -0500

    Created the plant class and cpp. Completed all getters and setters of the plant class.

---
+++ Ford-Assembly-Plant-Sec-2-Group-1/plant.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include "plant.h"
+
+using namespace std;
+
+Plant::Plant(string date, int globalAirQuality, int numVehicleToday, double globalTemp, double globalHumidity, int vehicleQuota, bool assemblyLineStatus)
+{
+	this->date = date;
+	this->globalAirQuality = globalAirQuality;
+	this->numVehicleToday = numVehicleToday;
+	this->globalTemp = globalTemp;
+	this->globalHumidity = globalHumidity;
+	this->vehicleQuota = vehicleQuota;
+	this->assemblyLineStatus = assemblyLineStatus;
+}
+
+Plant::Plant()
+{
+	date = "00/00/0000";
+	globalAirQuality = 0;
+	numVehicleToday = 0;
+	globalTemp = 0;
+	globalHumidity = 0;
+	vehicleQuota = 0;
+	assemblyLineStatus = false;
+}
+
+string Plant::getDate()
+{
+	return date;
+}
+
+int Plant::getGlobalAirQuality()
+{
+	return globalAirQuality;
+}
+
+int Plant::getNumVehicleToday()
+{
+	return numVehicleToday;
+}
+
+double Plant::getGlobalTemp()
+{
+	return globalTemp;
+}
+
+double Plant::getGlobalHumidity()
+{
+	return globalHumidity;
+}
+
+int Plant::getVehicleQuota()
+{
+	return vehicleQuota;
+}
+
+bool Plant::getAssemblyLineStatus()
+{
+	return assemblyLineStatus;
+}
+
+void Plant::setDate(string date)
+{
+	this->date = date;
+}
+
+void Plant::setGlobalAirQuality(int globalAirQuality)
+{
+	this->globalAirQuality = globalAirQuality;
+}
+
+void Plant::setNumVehicleToday(int numVehicleToday)
+{
+	this->numVehicleToday = numVehicleToday;
+}
+
+void Plant::setGlobalTemp(double globalTemp)
+{
+	this->globalTemp = globalTemp;
+}
+
+void Plant::setGlobalHumidity(double globalHumidity)
+{
+	this->globalHumidity = globalHumidity;
+}
+
+void Plant::setVehicleQuota(int vehicleQuota)
+{
+	this->vehicleQuota = vehicleQuota;
+}
+
+void Plant::setAssemblyLineStatus(bool assemblyLineStatus)
+{
+	this->assemblyLineStatus = assemblyLineStatus;
+}
+
+void Plant::runPlant()
+{
+
+}
---
+++ Ford-Assembly-Plant-Sec-2-Group-1/plant.h
@@ -0,0 +1,51 @@
+#pragma once
+
+using namespace std;
+
+#ifndef PLANT_H
+#define PLANT_H
+
+class Plant
+{
+	string date;
+	int globalAirQuality;
+	int numVehicleToday;
+	double globalTemp; //Rename into plantTemp when integration is complete
+	double globalHumidity; // rename into plantHumidity when integration is complete
+	int vehicleQuota;
+	bool assemblyLineStatus;
+
+public:
+
+	//PaintingMachine paintingMachine;
+	//BodyMachine bodyMachine;
+	//ChassisMachine chassisMachine;
+	//InteriorMachine interiorMachine;
+
+	Plant(string date, int globalAirQuality, int numVehicleToday, double globalTemp, double globalHumidity, int vehicleQuota, bool assemblyLineStatus);
+
+	Plant();
+
+	// Getters
+	string getDate();
+	int getGlobalAirQuality();
+	int getNumVehicleToday();
+	double getGlobalTemp();
+	double getGlobalHumidity();
+	int getVehicleQuota();
+	bool getAssemblyLineStatus();
+
+	// Setters
+	void setDate(string date);
+	void setGlobalAirQuality(int globalAirQuality);
+	void setNumVehicleToday(int numVehicleToday);
+	void setGlobalTemp(double globalTemp);
+	void setGlobalHumidity(double globalHumidity);
+	void setVehicleQuota(int vehicleQuota);
+	void setAssemblyLineStatus(bool assemblyLineStatus);
+
+	// 
+	void runPlant();
+}
+
+#endif // !PLANT_H
