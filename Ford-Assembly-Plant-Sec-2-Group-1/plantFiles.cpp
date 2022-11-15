9e6ef3b80afb797f811cc5e2507fea2225b25b1f
ZNLake <91035765+ZNLake@users.noreply.github.com>
2022-11-15 10:02:51 -0500

    Adjusted stopPLant, readlog and updateLog methods.

--- Ford-Assembly-Plant-Sec-2-Group-1/plantFiles.cpp
+++ Ford-Assembly-Plant-Sec-2-Group-1/plantFiles.cpp
@@ -7,38 +7,38 @@
 using namespace std;
 
 
-void updateLog(Plant plant[])
+void updateLog(Plant plant)
 {
 	ofstream plantSave;
 	plantSave.open("plantLog.txt", ios_base::app);
 
 	string date;
-	int i = 0;
+	//int i = 0;
 	int globalAirQuality, numVehicleToday, vehicleQuota;
 	double globalTemp,globalHumidity; 
 	bool assemblyLineStatus;
 
 	if (plantSave.is_open())
 	{	
-		while (plant[i].getDate().compare("00/00/0000") != 0)
+		while (plant.getDate().compare("00/00/0000") != 0)
 		{
-			date = plant[i].getDate();
-			globalAirQuality = plant[i].getGlobalAirQuality();
-			numVehicleToday = plant[i].getNumVehicleToday();
-			vehicleQuota = plant[i].getVehicleQuota();
-			globalTemp = plant[i].getGlobalTemp();
-			globalHumidity = plant[i].getGlobalHumidity();
-			assemblyLineStatus = plant[i].getAssemblyLineStatus();
+			date = plant.getDate();
+			globalAirQuality = plant.getGlobalAirQuality();
+			numVehicleToday = plant.getNumVehicleToday();
+			vehicleQuota = plant.getVehicleQuota();
+			globalTemp = plant.getGlobalTemp();
+			globalHumidity = plant.getGlobalHumidity();
+			assemblyLineStatus = plant.getAssemblyLineStatus();
 
 
 			plantSave << "\n --------------------\n" << " Date: " << date << "\n CO2 Concentration: " << globalAirQuality << "ppm\n" << "Number of vehicles made today: " << numVehicleToday << "\n Today's vehicle quota:  " << vehicleQuota << "\n Plant's global temperature: " << globalTemp << "F\n Plant's global humidity: " << globalHumidity << "%\n Assembly line running:" << assemblyLineStatus << "\n" << " --------------------\n";
-			i++;
+			//i++;
 		}
 	}
 	plantSave.close();
 }
 
-void readLog(Plant plant[])
+void readLog(Plant plant)
 {
 	ifstream plantLoad;
 	plantLoad.open("plantSensors.txt");
@@ -48,28 +48,29 @@ void readLog(Plant plant[])
 	int globalAirQuality, numVehicleToday, vehicleQuota;
 	double globalTemp, globalHumidity;
 	bool assemblyLineStatus;
-	int counter = 0;
+//	int counter = 0;
 
 	if (plantLoad.is_open())
 	{
 		while (plantLoad >> date >> globalAirQuality >> numVehicleToday >> vehicleQuota >> globalTemp >> globalHumidity >> assemblyLineStatus)
 		{
-			plant[counter].setDate(date);
-			plant[counter].setGlobalAirQuality(globalAirQuality);
-			plant[counter].setNumVehicleToday(numVehicleToday);
-			plant[counter].setVehicleQuota(vehicleQuota);
-			plant[counter].setGlobalTemp(globalTemp);
-			plant[counter].setGlobalHumidity(globalHumidity);
-			plant[counter].setAssemblyLineStatus(assemblyLineStatus);
+			plant.setDate(date);
+			plant.setGlobalAirQuality(globalAirQuality);
+			plant.setNumVehicleToday(numVehicleToday);
+			plant.setVehicleQuota(vehicleQuota);
+			plant.setGlobalTemp(globalTemp);
+			plant.setGlobalHumidity(globalHumidity);
+			plant.setAssemblyLineStatus(assemblyLineStatus);
 
-			counter++;
+			//counter++;
 		}
 	}
 	plantLoad.close();
 }
 
-void stopPlant(Plant plant[])
+void stopPlant(Plant plant)
 {
 	cout << "Plant is shutting down..." << endl;
 	updateLog(plant);
+	plant.setAssemblyLineStatus(false);
 }
