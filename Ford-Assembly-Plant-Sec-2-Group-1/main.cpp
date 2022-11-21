350e10405be702cba67eacae312c195fc5295656
Brodin Collins-Robb <Bcollinsrobb9037@conestogac.on.ca>
2022-11-21 15:50:50 -0500

    Cleared out main code so we can add interface

--- Ford-Assembly-Plant-Sec-2-Group-1/main.cpp
+++ Ford-Assembly-Plant-Sec-2-Group-1/main.cpp
@@ -6,23 +6,5 @@
 
 int main(void)
 {
-	InteriorMachine newInteriorMachine;//notably a new interior machine WILL NOT be created for every vehicle just for use in this test
-
-	//This information will normally be handeled in the plant object
-	Order placedOrder;
-	Vehicle vehiclePlaceholder;
-
-	placedOrder.getInteriorLevel() = "base";
-	placedOrder.getModel() = "F150";
-	vehiclePlaceholder.getInterior() = "N/A";
-
-	newInteriorMachine.SwitchVehiclePanelsBays("BayTwo");
-
-	newInteriorMachine.UpdateBaseInteriorF150InventoryAmount(100, "BayOne");
-
-	newInteriorMachine.SwitchVehiclePanelsBays("BayOne");
-
-	newInteriorMachine.RunInteriorMachine(placedOrder, &vehiclePlaceholder);
-
 	return 0;
 }
