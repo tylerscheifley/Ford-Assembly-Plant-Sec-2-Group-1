1dbb1a86c36dc4a7c275c25f0d1440f62da3df28
ZNLake <91035765+ZNLake@users.noreply.github.com>
2022-11-21 12:55:05 -0500

    Removed c# test folder and replaced with c++

---
+++ plantTests/plantTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace plantTests
+{
+	TEST_CLASS(plantTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
