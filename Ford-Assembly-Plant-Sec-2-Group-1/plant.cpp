6e5d1c0bcb594dc0617dd5c582154e990e87bd8a
ZNLake <91035765+ZNLake@users.noreply.github.com>
2022-11-21 12:31:34 -0500

    Removed Main from branch.

--- Ford-Assembly-Plant-Sec-2-Group-1/plant.cpp
+++ Ford-Assembly-Plant-Sec-2-Group-1/plant.cpp
@@ -114,3 +114,25 @@ void Plant::runPlant()
 	}
 
 }
+
+/*#include <iostream>
+#include "plant.h"
+#include "plantFiles.h"
+#include <windows.h>
+#define MAXPLANTS 4
+
+using namespace std;
+
+int main(void)
+{
+	Plant* plant = new Plant;
+	sampleDataCreator();
+	readLog(plant);
+	updateLog(plant);
+
+
+	//Plant* plant = new Plant[MAXPLANTS];
+	cout << "Plant" << endl;
+
+	return 0;
+}*/
