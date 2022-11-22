59b3eb8121d769c73ed807595eef2ffb7da6a99b
Brodin Collins-Robb <Bcollinsrobb9037@conestogac.on.ca>
2022-11-22 12:50:07 -0500

    Started Integration Test Suite

---
+++ Ford-Assembly-Plant-Integration-Tests/Ford-Assembly-Plant-Integration-Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FordAssemblyPlantIntegrationTests
+{
+	TEST_CLASS(FordAssemblyPlantIntegrationTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
